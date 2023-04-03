899fac49573abda503cb8490931a48f71fb1605d
Deepakkiriti <deepak@getpulse.in>
2023-04-03 22:18:52 +0530

    .

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { FaDyalog } from 'react-icons/fa';
+import ReactSvg from '../assets/react.svg';
 
 function About() {
+  const skills = [
+    "HTML5", "CSS", "Javascript", "reactjs", "bootstrap", "reactnative"
+  ]
   return (
-    <div>About</div>
-  )
+    <div className='text-center flex flex-col justify-center content-center mb-8'>
+      <div>
+        <h1 className='text-center text-white font-bold text-2xl font-serif'>About Me</h1>
+        <div className='flex felx-row justify-center content-center m-10'>
+          <div className='w-3/12 h-full float-left'>
+          <img src={ReactSvg} alt='React logo' className='w-3/6 h-3/6 float-left' />
+          </div>
+          <div className='w-9/12 h-full float-right'>
+          <p className='text-white font-general text-sl float-right mx-8 text-left uppercase'>
+            With one year of experience in React Native development, I have gained a deep understanding of the framework's capabilities and limitations.
+            React Native is a cross-platform mobile application development framework that allows developers to build applications for both iOS and Android platforms using JavaScript and React.
+            During my experience, I have honed my skills in building intuitive user interfaces, integrating third-party libraries and APIs, and troubleshooting issues.
+            In conclusion, my one-year experience in React Native development has provided me with a strong foundation in building cross-platform mobile applications.
+            I have gained proficiency in building intuitive user interfaces, integrating third-party libraries and APIs, and optimizing application performance.
+            I am excited to continue learning and growing in this field, and I am confident that my skills and experience make me a valuable asset to any development team.
+          </p>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col justify-center content-center text-center mt-10'>
+        <h1 className='text-center text-xl text-white font-serif font-bold uppercase'>Skills</h1>
+        <div className='flex flex-row justify-center content-center text-ceter m-10'>
+          {skills.map((skill) => (
+            <h1 className='text-white font-general text-lg mx-2 bg-indigo-900 p-3 rounded-xl uppercase'>{skill}</h1>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/Screens/Projects.jsx
+++ src/Screens/Projects.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
 
 function Projects() {
+  const projects = [
+    {
+      id: 1,
+      name: 'Pulse Mobile App',
+      img: 'https://cdn.dribbble.com/users/2345673/screenshots/16698256/media/888235710f030572a821871d233a92fe.png?compress=1&resize=400x300',
+      type: 'Mobile App',
+      description: 'Pulse is a mobile app for tracking your heart rate and other vitals.'
+    },
+    {
+      id: 2,
+      name: 'News App',
+      img: 'https://cdn.dribbble.com/users/1753227/screenshots/16459690/media/3ab03a98c5332034cfcf9c6dd8773eac.png?compress=1&resize=400x300',
+      type: 'Mobile App',
+      description: 'A news app that aggregates news from various sources and displays them in a user-friendly interface.'
+    },
+    {
+      id: 3,
+      name: 'Student Result Management',
+      img: 'https://cdn.dribbble.com/users/861271/screenshots/15508543/media/9490f146ac3fe7c1d38561a4fc32ae6d.png?compress=1&resize=400x300',
+      type: 'Website',
+      description: 'A web-based application for managing student grades, attendance, and other related information.'
+    },
+    {
+      id:4,
+      name:"Petspot",
+      img:"https://cdn.dribbble.com/users/8430061/screenshots/17764839/media/0192f34031fe4fdf35cfb266465e9b6a.png?compress=1&resize=400x300&vertical=top",
+      type:"website",
+      description:"a web-based application for purchase of Pets with a hude Discount price"
+    }
+  ];
+
   return (
-    <div>Projects</div>
-  )
+    <div className='w-full h-full justify-center content-center text-center flex my-20 px-10'>
+      <div className='flex flex-col justify-center content-center text-center space-y-10'>
+        <h1 className='text-center text-white font-bold text-2xl font-serif'>Projects</h1>
+        <div className='flex felx-row'>
+        {projects.map((project , index ) => (
+        <div className='bg-slate-900 rounded-xl p-4 m-3 flex felx-row '>
+              <div className='flex flex-col' key={index}>
+                <div className='relative'>
+                  <img src={project.img} alt={project.name} className='w-full h-full rounded-xl' />
+                  <div className='absolute inset-0 bg-black opacity-0 hover:opacity-90 rounded-xl transition-opacity'>
+                    <div className='flex items-center justify-center h-full'>
+                      <p className='text-white text-center'>{project.description}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='flex flex-col justify-center'>
+                  <h1 className='text-white font-bold uppercase'>{project.name}</h1>
+                  <h2 className='text-white uppercase'>{project.type}</h2>
+                </div>
+              </div>
+          </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
