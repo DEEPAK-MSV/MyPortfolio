502247fc6455ed43e49c5f73e612b7605bfeb212
Deepakkiriti <deepak@getpulse.in>
2023-04-05 12:40:51 +0530

    .

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,8 +17,8 @@ const routes = [
 
 function Header() {
   return (
-    <div className='w-full justify-center content-center flex'>
-      <div className='w-full justify-between content-center my-5 flex flex-row cursor-default'>
+    <div className='w-full justify-center items-center flex'>
+      <div className='w-full justify-between items-center my-5 flex flex-row cursor-default'>
         <Link to='/'>
           <div className='flex flex-row mx-5 cursor-default'>
             <FaDyalog className='text-3xl font-bold font-serif text-white text-center drop-shadow-2xl' />
@@ -26,16 +26,16 @@ function Header() {
           </div>
         </Link>
         <div className='flex flex-row space-x-10'>
-        <div className='flex-row flex cursor-pointer space-x-3'>
+        <div className='flex-row flex cursor-pointer items-center space-x-3'>
           {routes.map((route) => (
             <NavLink key={route.id} to={route.path}>
-              <h1 className='text-xl font-bold font-serif text-white text-center mx-2'>{route.name}</h1>
+              <h1 className='text-xl font-bold items-center font-serif text-white text-center mx-2'>{route.name}</h1>
             </NavLink>
           ))}
         </div>
-        <div className='sd:hidden pb-3'>
+        <div className='sd:hidden items-center pb-3'>
           <a href='mailto:deepakkiriti.m.s.v@gmail.com'>
-          <button className='bg-indigo-600 border border-indigo-900 rounded mx-4 px-3 py-2 drop-shadow-lg cursor-default font-serif text-white font-bold hover:bg-indigo-900 hover:text-white'>Hire Me</button>
+          <button className='bg-indigo-600 border border-indigo-900 items-center rounded mx-4 px-3 py-2 drop-shadow-lg cursor-default font-serif text-white font-bold hover:bg-indigo-900 hover:text-white'>Hire Me</button>
           </a>
         </div>
         </div>
