70b20d4bde2f1a900ea8b72435c984db52ab2b4d
Deepak <111272193+DEEPAK-MSV@users.noreply.github.com>
2025-06-10 13:20:40 +0530

    Update HomePage.jsx

--- src/Screens/HomePage.jsx
+++ src/Screens/HomePage.jsx
@@ -8,7 +8,7 @@ function HomePage() {
         <div className='w-full h-full mb-20'>
             <div className='w-full h-full justify-center items-center flex flex-col md:flex-row lg:flex-row'>
                 <div className='justify-center items-center flex flex-col mt-10 space-y-4 lg:w-6/12 '>
-                    <h1 className='text-2xl lg:text-4xl font-bold text-white uppercase text-center'>hi, iam Deepak</h1>
+                    <h1 className='text-2xl lg:text-4xl font-bold text-white uppercase text-center'>hi, i am Deepak</h1>
                     <p className='text-lg lg:text-3xl font-bold font-serif text-white uppercase text-center flex-nowrap'>Front End developer</p>
                     <a download='Deepak_Angular_developer.pdf' href='https://drive.google.com/uc?export=download&id=1mYjBlll1J_4jQ4wmr6husqI4XOvDLatT' >
                         <button className='bg-indigo-400 mb-3 rounded p-3 drop-shadow-lg hover:bg-indigo-900 hover:text-white duration-100 flex flex-row text-center justify-center content-center'>
@@ -17,7 +17,7 @@ function HomePage() {
                         </button>
                     </a>
                 </div>
-                <div className='w-full h-full opacity-70 justify-center items-center flex'>
+                <div className='w-full h-70 opacity-70 justify-center items-center flex'>
                     <img src={devloper} className='w-full h-full rounded-lg drop-shadow-lg' />
                 </div>
             </div>
